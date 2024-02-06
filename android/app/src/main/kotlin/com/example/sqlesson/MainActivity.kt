42f4f129ba84ecf2ae86ec719b1d32cdfb8a297f
pikaparson <sonna1012@gmail.com>
2024-02-06 17:15:46 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sqlesson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqlesson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sql_helper.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+//создание таблицы
+class SQLHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+// id: the id of a item
+// title, description: name and description of your activity
+// created_at: the time that the item was created. It will be automatically handled by SQLite
+
+  //создание конкретной таблицы
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'kindacode.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  // Создание нового объекта (journal)
+  static Future<int> createItem(String title, String? descrption) async {
+    final db = await SQLHelper.db();
+
+    final data = {'title': title, 'description': descrption};
+    final id = await db.insert('items', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  // чтение всех объектов (journals)
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await SQLHelper.db();
+    return db.query('items', orderBy: "id");
+  }
+
+  // чтение одного объекта по идентификатору
+  // Приложение не использует этот метод, но он здесь на случай
+  static Future<List<Map<String, dynamic>>> getItem(int id) async {
+    final db = await SQLHelper.db();
+    return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  // Обновление объекта по идентификатору
+  static Future<int> updateItem(
+      int id, String title, String? descrption) async {
+    final db = await SQLHelper.db();
+
+    final data = {
+      'title': title,
+      'description': descrption,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result =
+    await db.update('items', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  // Удаление объекта
+  static Future<void> deleteItem(int id) async {
+    final db = await SQLHelper.db();
+    try {
+      await db.delete("items", where: "id = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting an item: $err");
+    }
+  }
+}
